fbdf361f28ed2033132f3d5687dd8ed10d03a84d
Rafael Duarte Mauricio Correia <a55910@alunos.ipb.pt>
2023-10-25 17:16:18 +0100

    Adição de tipos aos etilos

--- types/Styles.ts
+++ types/Styles.ts
@@ -1,5 +1,23 @@
-import { ViewStyle } from "react-native";
+import { ImageStyle, ViewStyle } from "react-native";
 
-export type Styles = {
+export type HomeScreenStyles = {
   container: ViewStyle;
+  subContainerFilter: ViewStyle;
+  subContainerNavBar: ViewStyle;
+};
+
+export type NavBarStyles = {
+  container: ViewStyle;
+  navBarContainer: ViewStyle;
+  logo: ImageStyle;
+};
+
+export type CategoryDropdownFilterStyles = {
+  dropDownContainer: ViewStyle;
+  container: ViewStyle;
+};
+
+export type MovieNameFilterInputStyles = {
+  input: ViewStyle;
+  searchIcon: ViewStyle;
 };
