8ef2b0e32c49fa77b0e20e5a8a3ac63fa38cd317
Rafael Duarte Mauricio Correia <a55910@alunos.ipb.pt>
2023-10-25 01:41:52 +0100

    melhor estruturamento do codigo

---
+++ types/Props.ts
@@ -0,0 +1,9 @@
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import {
+  CompositeNavigationProp,
+  NavigationProp,
+} from "@react-navigation/native";
+
+export interface HomeScreenProps {
+  navigation: NavigationProp<ReactNavigation.RootParamList>;
+}
---
+++ utils/functions.ts
@@ -0,0 +1,5 @@
+import { IThemeColor } from "../types/interfaces/IThemeColor";
+
+export function getColorsFromContext(colors: IThemeColor) {
+  return colors;
+}
