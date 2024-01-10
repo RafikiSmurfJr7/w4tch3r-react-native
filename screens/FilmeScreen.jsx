df0bcc739e372b869486fbc62f96874dcba6e1b2
Pedro Miguel Carvalho Pinto <a55905@alunos.ipb.pt>
2024-01-10 16:45:34 +0000

    switch novo e biblioteca do switch

--- screens/FilmeScreen.jsx
+++ screens/FilmeScreen.jsx
@@ -5,60 +5,70 @@ import { ThemeColor } from "../context/ThemeColor";
 import MoviesTopRatedList from "../components/movies/MovieTopRatedList";
 import MovieSearchList from "../components/movies/MovieSearchList";
 import MoviesPopularList from "../components/movies/MoviePopularList";
+import SwitchSelector from "react-native-switch-selector";
+
 
 export default function FilmeScreen({ navigation }) {
-  const colors = useContext(ThemeColor);
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: colors.blue,
-    },
-    subContainerNavBar: {},
-    subContainerFilter: {},
-    listContainer: {
-      flex: 1,
-      marginTop: 10,
-    },
-    switchContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 10,
-    },
-    switchLabel: {
-      color: colors.white,
-      marginRight: 10,
-    },
-  });
-  const [movieName, setMovieName] = useState("");
-  const [showTopRated, setShowTopRated] = useState(true);
+    const colors = useContext(ThemeColor);
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.subContainerNavBar}>
-        <NavBar />
-      </View>
-      {/* <View style={styles.subContainerFilter}>
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: colors.blue,
+        },
+        subContainerNavBar: {},
+        subContainerFilter: {},
+        listContainer: {
+            flex: 1,
+            marginTop: 10,
+        },
+        switchContainer: {
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: 10,
+        },
+        switchLabel: {
+            color: colors.white,
+            marginRight: 10,
+        },
+    });
+    const [movieName, setMovieName] = useState("");
+    const [showTopRated, setShowTopRated] = useState(true);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.subContainerNavBar}>
+                <NavBar />
+            </View>
+            {/* <View style={styles.subContainerFilter}>
         <MovieFilters setMovieName={setMovieName} movieName={movieName} />
       </View> */}
-      <View style={styles.switchContainer}>
-        <Text style={styles.switchLabel}>Popular</Text>
-        <Switch
-          value={showTopRated}
-          onValueChange={() => setShowTopRated(!showTopRated)}
-        />
-        <Text style={styles.switchLabel}>  Top Rated</Text>
-      </View>
-      <View style={styles.listContainer}>
-        {movieName !== "" ? (
-          <MovieSearchList movieName={movieName} />
-        ) : showTopRated ? (
-          <MoviesTopRatedList />
-        ) : (
-          <MoviesPopularList />
-        )}
-      </View>
-    </View>
-  );
+            <View style={styles.switchContainer}>
+                <SwitchSelector
+                    initial={0}
+                    textColor={"#000000"}
+                    selectedColor={"#fff"}
+                    buttonColor={colors.blue}
+                    borderColor={"#ffffff"}
+                    valuePadding={2}
+                    hasPadding
+                    onPress={(value) => setShowTopRated(!showTopRated)}
+                    options={[
+                        { label: "Top Rated", value: false },
+                        { label: "Popular", value: false },
+                    ]}
+                />
+            </View>
+            <View style={styles.listContainer}>
+                {movieName !== "" ? (
+                    <MovieSearchList movieName={movieName} />
+                ) : showTopRated ? (
+                    <MoviesTopRatedList />
+                ) : (
+                    <MoviesPopularList />
+                )}
+            </View>
+        </View>
+    );
 }
