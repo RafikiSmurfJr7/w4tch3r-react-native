708e22c616654a07c1bb968fa307c857ea6b374d
Pedro Miguel Carvalho Pinto <a55905@alunos.ipb.pt>
2024-01-15 19:10:44 +0000

    navegação arranjada

--- routes/DrawerRoute.jsx
+++ routes/DrawerRoute.jsx
@@ -1,10 +1,10 @@
 import { View, Text, ActivityIndicator } from "react-native";
 import React, {
-  createContext,
-  useEffect,
-  useMemo,
-  useReducer,
-  useState,
+    createContext,
+    useEffect,
+    useMemo,
+    useReducer,
+    useState,
 } from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "../screens/HomeScreen";
@@ -24,117 +24,132 @@ import { AuthContext } from "../context/AuthContext";
 const Drawer = createDrawerNavigator();
 
 export default function DrawerRoute() {
-  const [state, dispatch] = useReducer(
-    (prevState, action) => {
-      switch (action.type) {
-        case "RESTORE_TOKEN":
-          return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
-          };
-        case "SIGN_IN":
-          return {
-            ...prevState,
+    const [state, dispatch] = useReducer(
+        (prevState, action) => {
+            switch (action.type) {
+                case "RESTORE_TOKEN":
+                    return {
+                        ...prevState,
+                        userToken: action.token,
+                        isLoading: false,
+                    };
+                case "SIGN_IN":
+                    return {
+                        ...prevState,
+                        isSignout: false,
+                        userToken: action.token,
+                    };
+                case "SIGN_OUT":
+                    return {
+                        ...prevState,
+                        isSignout: true,
+                        userToken: null,
+                    };
+            }
+        },
+        {
+            isLoading: true,
             isSignout: false,
-            userToken: action.token,
-          };
-        case "SIGN_OUT":
-          return {
-            ...prevState,
-            isSignout: true,
             userToken: null,
-          };
-      }
-    },
-    {
-      isLoading: true,
-      isSignout: false,
-      userToken: null,
-    }
-  );
+        }
+    );
 
-  useEffect(() => {
-    const bootstrapAsync = async () => {
-      let userToken;
+    useEffect(() => {
+        const bootstrapAsync = async () => {
+            let userToken;
 
-      try {
-        userToken = await SecureStore.getItemAsync("access_token");
-      } catch (e) {
-        console.log(e);
-      }
+            try {
+                userToken = await SecureStore.getItemAsync("access_token");
+            } catch (e) {
+                console.log(e);
+            }
 
-      dispatch({ type: "RESTORE_TOKEN", token: userToken });
+            dispatch({ type: "RESTORE_TOKEN", token: userToken });
+        };
+
+        bootstrapAsync();
+    }, []);
+
+    const authContext = {
+        signIn: async (data) => {
+            let userToken = null;
+
+            backendApi
+                .post("/auth/login", {
+                    username: data.username,
+                    password: data.password,
+                })
+                .then(async (res) => {
+                    console.log(res.data.token);
+
+                    userToken = res.data.token;
+                    try {
+                        await SecureStore.setItemAsync(
+                            "access_token",
+                            String(userToken)
+                        );
+                    } catch (error) {
+                        console.log(error);
+                    }
+
+                    console.log(userToken);
+
+                    dispatch({ type: "SIGN_IN", token: userToken });
+                })
+                .catch((err) => {
+                    console.log(err.message);
+                });
+        },
+        signOut: async () => {
+            try {
+                await SecureStore.deleteItemAsync("access_token");
+            } catch (error) {
+                console.log(error);
+            }
+            dispatch({ type: "SIGN_OUT" });
+        },
     };
 
-    bootstrapAsync();
-  }, []);
+    if (state.isLoading) {
+        return <ActivityIndicator size="large" color="#ffffff" />;
+    }
 
-  const authContext = {
-    signIn: async (data) => {
-      let userToken = null;
-
-      backendApi
-        .post("/auth/login", {
-          username: data.username,
-          password: data.password,
-        })
-        .then(async (res) => {
-          console.log(res.data.token);
-
-          userToken = res.data.token;
-          try {
-            await SecureStore.setItemAsync("access_token", String(userToken));
-          } catch (error) {
-            console.log(error);
-          }
-
-          console.log(userToken);
-
-          dispatch({ type: "SIGN_IN", token: userToken });
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-    },
-    signOut: async () => {
-      try {
-        await SecureStore.deleteItemAsync("access_token");
-      } catch (error) {
-        console.log(error);
-      }
-      dispatch({ type: "SIGN_OUT" });
-    },
-  };
-
-  if (state.isLoading) {
-    return <ActivityIndicator size="large" color="#ffffff" />;
-  }
-
-  return (
-    <AuthContext.Provider value={authContext}>
-      <Drawer.Navigator
-        screenOptions={{ headerShown: false }}
-        initialRouteName="Home"
-        drawerContent={() => <DrawerMenu />}
-      >
-        {state.userToken != null ? (
-          <>
-            <Drawer.Screen name="Home" component={HomeScreen} />
-            <Drawer.Screen name="Serie" component={SerieScreen} />
-            <Drawer.Screen name="SerieDetail" component={SerieDetailScreen} />
-            <Drawer.Screen name="Filme" component={FilmeScreen} />
-            <Drawer.Screen name="FilmeDetail" component={FilmeDetailScreen} />
-            <Drawer.Screen name="People" component={PeopleScreen} />
-            <Drawer.Screen name="PeopleDetail" component={PeopleDetailScreen} />
-            <Drawer.Screen name="FavoriteFilm" component={FavoriteFilmScreen} />
-          </>
-        ) : (
-          <>
-            <Drawer.Screen name="Login" component={LoginScreen} />
-          </>
-        )}
-      </Drawer.Navigator>
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={authContext}>
+            <Drawer.Navigator
+                screenOptions={{ headerShown: false }}
+                backBehavior="history"
+                drawerContent={() => <DrawerMenu />}
+            >
+                {state.userToken != null ? (
+                    <>
+                        <Drawer.Screen name="Home" component={HomeScreen} />
+                        <Drawer.Screen name="Serie" component={SerieScreen} />
+                        <Drawer.Screen
+                            name="SerieDetail"
+                            component={SerieDetailScreen}
+                        />
+                        <Drawer.Screen name="Filme" component={FilmeScreen} />
+                        <Drawer.Screen
+                            name="FilmeDetail"
+                            component={FilmeDetailScreen}
+                        />
+                        <Drawer.Screen name="People" component={PeopleScreen} />
+                        <Drawer.Screen
+                            name="PeopleDetail"
+                            component={PeopleDetailScreen}
+                        />
+                        <Drawer.Screen
+                            name="FavoriteFilm"
+                            component={FavoriteFilmScreen}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Drawer.Screen name="Login" component={LoginScreen} />
+                    </>
+                )}
+            </Drawer.Navigator>
+        </AuthContext.Provider>
+    );
 }
