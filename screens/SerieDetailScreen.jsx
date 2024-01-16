1f744fe71fd65c725a6d6d5e2ed96448ac290915
Rafael Duarte Mauricio Correia <a55910@alunos.ipb.pt>
2024-01-16 23:23:07 +0000

    clear serie details

--- screens/SerieDetailScreen.jsx
+++ screens/SerieDetailScreen.jsx
@@ -9,11 +9,11 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
 import { Iframe } from "@bounceapp/iframe";
 import { useRoute } from "@react-navigation/native";
 import NavBar from "../components/NavBar";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import SerieSeasonEpisodeButton from "../components/series/SerieSeasonEpisodeButton";
 import SerieEpisodeButton from "../components/series/SerieEpisodeButton";
 import { tmdbApi } from "../config/axios.conf";
@@ -26,8 +26,6 @@ const SerieDetailScreen = ({ navigation }) => {
   const [showEpisodes, setShowEpisodes] = useState(false);
   const [episodesArray, setEpisodesArray] = useState([0]);
   const [episode, setEpisode] = useState(1);
-  const [isWatchLaterClicked, setIsWatchLaterClicked] = useState(false);
-  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     setShowEpisodes(false);
@@ -40,59 +38,6 @@ const SerieDetailScreen = ({ navigation }) => {
       .catch((err) => {});
   }, [route.params.id]);
 
-  useEffect(() => {
-    const loadFavorites = async () => {
-      try {
-        const storedFavorites = await AsyncStorage.getItem('favorites');
-        if (storedFavorites) {
-          setFavorites(JSON.parse(storedFavorites));
-        }
-      } catch (error) {
-        console.error('Error loading favorites:', error);
-      }
-    };
-
-    loadFavorites();
-  }, []);
-
-  const isFavorite = favorites.some(fav => fav.id === serieData?.id);
-
-  const toggleFavorite = async () => {
-    try {
-      let updatedFavorites = [...favorites];
-
-      const isAlreadyFavorite = updatedFavorites.some(fav => fav.id === serieData.id);
-
-      if (isAlreadyFavorite) {
-        updatedFavorites = updatedFavorites.filter(fav => fav.id !== serieData.id);
-      } else {
-        updatedFavorites.push({
-          id: serieData.id,
-          name: serieData.name,
-          poster_path: serieData.poster_path,
-          vote_average: serieData.vote_average,
-      
-        });
-      }
-
-      await AsyncStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-      setFavorites(updatedFavorites);
-    } catch (error) {
-      console.error('Error manipulating favorites:', error);
-    }
-  };
-
-  const checkIsWatchLater = async (id) => {
-    try {
-      const storedWatchLater = await AsyncStorage.getItem('watchLaterSeries');
-      const watchLaterList = storedWatchLater ? JSON.parse(storedWatchLater) : [];
-      const isAlreadyInWatchLater = watchLaterList.some(item => item.id === id);
-      setIsWatchLaterClicked(isAlreadyInWatchLater);
-    } catch (error) {
-      console.error('Error checking if series is in "Watch Later":', error);
-    }
-  };
-
   const handleSerieButtonPressed = (seasonNumber) => {
     setShowEpisodes(false);
     setSeason(parseInt(seasonNumber));
@@ -102,7 +47,9 @@ const SerieDetailScreen = ({ navigation }) => {
       episodeCount = 0;
     }
     let episodeNumbers = 0;
-    if (serieData.seasons[parseInt(seasonNumber) + episodeCount].episode_count) {
+    if (
+      serieData.seasons[parseInt(seasonNumber) + episodeCount].episode_count
+    ) {
       episodeNumbers =
         serieData.seasons[parseInt(seasonNumber) + episodeCount].episode_count;
     }
@@ -117,32 +64,6 @@ const SerieDetailScreen = ({ navigation }) => {
     setEpisode(parseInt(episodeNumber));
   };
 
-  const handleWatchLater = async () => {
-    try {
-      const storedWatchLater = await AsyncStorage.getItem('watchLaterSeries');
-      let watchLaterList = storedWatchLater ? JSON.parse(storedWatchLater) : [];
-
-      const isAlreadyInWatchLater = watchLaterList.some(item => item.id === serieData.id);
-
-      if (isAlreadyInWatchLater) {
-        watchLaterList = watchLaterList.filter(item => item.id !== serieData.id);
-      } else {
-        watchLaterList.push({
-          id: serieData.id,
-          name: serieData.name,
-          poster_path: serieData.poster_path,
-          vote_average: serieData.vote_average,
-        
-        });
-      }
-
-      await AsyncStorage.setItem('watchLaterSeries', JSON.stringify(watchLaterList));
-      checkIsWatchLater(serieData.id);
-    } catch (error) {
-      console.error('Error manipulating "Watch Later" for series:', error);
-    }
-  };
-
   const customStyles = StyleSheet.create({
     container: {
       flex: 1,
@@ -154,12 +75,6 @@ const SerieDetailScreen = ({ navigation }) => {
     imageTransparent: {
       flex: 1,
       height: "100%",
-    },
-    starContainer: {
-      position: 'absolute',
-      top: 70,
-      right: 40,
-      zIndex: 1,
     },
     serieInfoContainer: {
       flexDirection: "row",
@@ -239,22 +154,7 @@ const SerieDetailScreen = ({ navigation }) => {
               style={customStyles.imageTransparent}
             >
               <NavBar />
-              <TouchableOpacity
-                onPress={toggleFavorite}
-                style={customStyles.starContainer}
-              >
-                <Icon
-                  name={isFavorite ? 'star' : 'star-outline'}
-                  size={30}
-                  color="yellow"
-                />
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={handleWatchLater}
-                style={{ position: 'absolute', top: 72, right: 80, zIndex: 1 }}
-              >
-                <Icon name="time" size={30} color={isWatchLaterClicked ? 'orange' : 'white'} />
-              </TouchableOpacity>
+
               <View style={customStyles.serieInfoContainer}>
                 <Image
                   source={{
@@ -262,8 +162,10 @@ const SerieDetailScreen = ({ navigation }) => {
                   }}
                   style={customStyles.poster}
                 />
-                <View style={{ alignSelf: 'center', width: 150 }}>
-                  <Text style={customStyles.serieInfoText}>{serieData.name}</Text>
+                <View style={{ alignSelf: "center", width: 150 }}>
+                  <Text style={customStyles.serieInfoText}>
+                    {serieData.name}
+                  </Text>
                   <Text style={customStyles.serieInfoText}>
                     ({serieData.first_air_date.slice(0, 4)})
                   </Text>
@@ -271,25 +173,27 @@ const SerieDetailScreen = ({ navigation }) => {
                     Rating: {serieData.vote_average.toPrecision(2) * 10}%
                   </Text>
                   <Text style={customStyles.serieInfoText}>
-                    Genre:{' '}
+                    Genre:{" "}
                     {serieData.genres.map((item, i) =>
                       i < serieData.genres.length - 1
                         ? `  ${item.name} ,`
                         : `  ${item.name} `
-                    )}{' '}
+                    )}{" "}
                   </Text>
                   <Text style={customStyles.serieInfoText}>
-                    Created by:{' '}
+                    Created by:{" "}
                     {serieData.created_by.map((item, i) =>
                       i < serieData.created_by.length - 1
                         ? `  ${item.name} ,`
                         : `  ${item.name} `
-                    )}{' '}
+                    )}{" "}
                   </Text>
                 </View>
               </View>
               <View style={customStyles.serieDescriptionContainer}>
-                <Text style={customStyles.serieInfoText}>{serieData.overview}</Text>
+                <Text style={customStyles.serieInfoText}>
+                  {serieData.overview}
+                </Text>
               </View>
             </ImageBackground>
           </ImageBackground>
@@ -313,8 +217,12 @@ const SerieDetailScreen = ({ navigation }) => {
               </View>
               {showEpisodes ? (
                 <>
-                  <Text style={customStyles.seriePlayerEpisodesTitle}>Episodes</Text>
-                  <View style={customStyles.seriePlayerEpisodesButtonsContainer}>
+                  <Text style={customStyles.seriePlayerEpisodesTitle}>
+                    Episodes
+                  </Text>
+                  <View
+                    style={customStyles.seriePlayerEpisodesButtonsContainer}
+                  >
                     {episodesArray.map((item, i) => (
                       <SerieEpisodeButton
                         handleSerieEpisodeButtonPressed={
@@ -342,6 +250,6 @@ const SerieDetailScreen = ({ navigation }) => {
       )}
     </View>
   );
-}
+};
 
 export default SerieDetailScreen;
